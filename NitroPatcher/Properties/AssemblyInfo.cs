d338e7b8577507677f27e8892f2f3f56aede8518
Xzonn <Xzonn@outlook.com>
2024-10-11 20:32:53 +0800

    Update version

--- NitroPatcher/Properties/AssemblyInfo.cs
+++ NitroPatcher/Properties/AssemblyInfo.cs
@@ -39,7 +39,7 @@ using System.Windows;
 )]
 
 
-// 程序集的版本信息由下列四个值组成: 
+// 程序集的版本信息由下列四个值组成:
 //
 //      主版本
 //      次版本
@@ -49,5 +49,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.1")]
-[assembly: AssemblyFileVersion("1.3.1")]
+[assembly: AssemblyVersion("1.3.3")]
+[assembly: AssemblyFileVersion("1.3.3")]
